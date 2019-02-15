f5456dff7097419e02e2023bafb1ba9cceda07e3
leekhanh1993 <lkgame1993@gmail.com>
2019-02-15 23:13:31 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Lead Manager
+React-Django
---
+++ leadmanager/frontend/src/components/lead/Leads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Leads extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add 1 Leads</h1>
+            </div>
+        );
+    }
+}
+
+export default Leads;
